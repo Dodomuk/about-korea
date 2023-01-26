0ac8ae7c54ad090d2bd08c5b72cf38f8e441c196
정동묵 <siderid94@gmail.com>
2023-01-26 22:07:04 +0900

    sgis api 연동

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  // <React.StrictMode> //개발 모드 검사 도구
     <App />
-  </React.StrictMode>,
+  // </React.StrictMode>,
 )
