99180a5e16266dc6c733778f16acdd723937d40d
정동묵 <siderid94@gmail.com>
2023-01-30 22:30:38 +0900

    recoil 중복호출 방지 및 일부 상태 관리 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,27 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import { BrowserRouter } from 'react-router-dom'
 import axios from "axios";
+
+import { BrowserRouter } from 'react-router-dom'
 import { isFormElement } from 'react-router-dom/dist/dom';
 
-function App(){
-  const [count, setCount] = useState(0)
-  const [isApiCall, setIsApiCall] = useState(false)
+import Main from "./components/index";
 
-  useEffect(() => { 
-    if (isApiCall) { 
-      console.log('이미 한번 호출함');
-    } else{
-    axios.get(`${import.meta.env.VITE_SGIS_URL}/OpenAPI3/auth/authentication.json`, {
-      params: {
-        consumer_key: import.meta.env.VITE_SGIS_SERVICE_ID,
-        consumer_secret: import.meta.env.VITE_SGIS_API_KEY
-      }
-    }).then((response) => { 
-      console.log('응답부', response);
-      setCount(100);
-      setIsApiCall(true);
-    })
-  }
-  });
+import './App.css'
+import reactLogo from './assets/react.svg'
+import { RecoilRoot, RecoilEnv } from 'recoil';
+
+function App() {
+  
+  //Recoil 중복 호출 에러 방지
+  RecoilEnv.RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED = false;
 
   return (
     <div className="App">
       <BrowserRouter>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-          sgis api 발급< br/>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-        </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-        </p>
-        </BrowserRouter>
+      <RecoilRoot>  
+          <Main />
+      </ RecoilRoot>  
+      </BrowserRouter>
     </div>
   )
 }
