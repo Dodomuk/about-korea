09df84ebff37bc9d9c5483167544ff75341b77c8
정동묵 <siderid94@gmail.com>
2023-02-01 22:32:21 +0900

    tsx 파일 prettier 적용 안되는 현상 해결

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,42 +1,48 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { atom, RecoilRoot, useRecoilState } from "recoil"
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { atom, RecoilRoot, useRecoilState } from 'recoil';
 
-function Main() { 
-    const returnKey = atom({
-        key: 'returnKey',
-        default: ''
-    });
+function Main() {
+  const returnKey = atom({
+    key: 'returnKey',
+    default: '',
+  });
 
-    const [count, setCount] = useState(0)
-    const [isApiCall, setIsApiCall] = useState(false)
-    const [key, setKey] = useRecoilState(returnKey)
-  
-    useEffect(() => { 
-      if (isApiCall) { 
-        console.log('이미 한번 호출함');
-      } else{
-      axios.get(`${import.meta.env.VITE_SGIS_URL}/OpenAPI3/auth/authentication.json`, {
-        params: {
-          consumer_key: import.meta.env.VITE_SGIS_SERVICE_ID,
-          consumer_secret: import.meta.env.VITE_SGIS_API_KEY
-        }
-      }).then((response) => { 
-        console.log('응답부', response.data);
+  const [count, setCount] = useState(0);
+  const [isApiCall, setIsApiCall] = useState(false);
+  const [key, setKey] = useRecoilState(returnKey);
+
+  useEffect(() => {
+    if (isApiCall) {
+      console.log('이미 한번 호출함');
+    } else {
+      axios
+        .get(
+          `${import.meta.env.VITE_SGIS_URL}/OpenAPI3/auth/authentication.json`,
+          {
+            params: {
+              consumer_key: import.meta.env.VITE_SGIS_SERVICE_ID,
+              consumer_secret: import.meta.env.VITE_SGIS_API_KEY,
+            },
+          },
+        )
+        .then((response) => {
+          console.log('응답부', response.data);
           setIsApiCall(true);
           setKey(response.data.result.accessToken);
-      })
-      }
-    });      
+        });
+    }
+  });
 
-    return (
-            <div>
-          <button onClick={() => setCount((count) => count + 1)}>
-                sgis api 발급 AccessKey : {key}< br/>
-                count is {count}
-                </button>
-                </div>
-    )
+  return (
+    <div>
+      <button onClick={() => setCount((count) => count + 1)}>
+        sgis api 발급 AccessKey : {key}
+        <br />
+        count is {count}
+      </button>
+    </div>
+  );
 }
 
-export default Main
+export default Main;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   // <React> //개발 모드 검사 도구
-    <App />
+  <App />,
   //  </React.StrictMode>,
-)
+);
