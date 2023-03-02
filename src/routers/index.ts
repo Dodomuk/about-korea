f2f5f514116476471f9bc25c1ec722ec603d40fb
정동묵 <siderid94@gmail.com>
2023-03-02 14:43:51 +0900

    This is a combination of 3 commits.
    rebase #1

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
+import { isBooleanObject } from 'util/types';
 
 import Main from '../components/index';
 // const main = React.lazy(() => import('../components/index'));
@@ -10,3 +11,5 @@ export const routes = createBrowserRouter([
     element: Main, // (대만 후 처리)
   },
 ]);
+dodomuck = babo 3 / 2
+}
