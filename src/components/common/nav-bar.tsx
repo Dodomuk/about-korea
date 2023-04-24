c04a963ab1d0ea4b168e23e3b54ed07b34544bd6
정동묵 <siderid94@gmail.com>
2023-04-24 22:59:12 +0900

    navbar center 정렬

--- src/components/common/nav-bar.tsx
+++ src/components/common/nav-bar.tsx
@@ -12,7 +12,7 @@ const NavBar = () => {
     const itemList = contentsList['센서스'].flatMap((item) => item.title.replace('통계', ''));
 
     const navList = (
-        <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+        <ul className="mb-4 mt-2 lg:mb-0 lg:mt-0 flex flex-col lg:flex-row gap-2 items-center lg:gap-6">
             {itemList.map((item) => (
                 <Typography as="li" variant="small" color="blue-gray" className="p-1 font-normal" key={`통계_${item}`}>
                     <a href="#" className="flex items-center">
@@ -24,10 +24,8 @@ const NavBar = () => {
     );
 
     return (
-        <Navbar className="mx-auto max-w-screen-xl py-2 px-4 lg:px-8 lg:py-4">
-            <div className="container mx-auto flex items-center justify-between text-blue-gray-900">
-                <div className="hidden lg:block">{navList}</div>
-            </div>
+        <Navbar className="fixed top-0 left-0 right-0 mx-auto max-w-max py-2 px-4 lg:px-8 lg:py-4">
+            <div className="hidden lg:block">{navList}</div>
             <IconButton
                 variant="text"
                 className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
